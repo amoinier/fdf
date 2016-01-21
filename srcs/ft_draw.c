2f5cc0982fef9eeba960ac4d73c1ffbec8bca579
Alex <Alex@MacBook-Alex.local>
2016-01-21 22:23:36 +0100

    server de merde ! + drawline

--- srcs/ft_draw.c
+++ srcs/ft_draw.c
@@ -37,14 +37,72 @@ void	draw42(t_env *init, t_point ***tab, int col)
 	}
 }
 
+void	line1(t_env *init, int dxy[2], int xyi[2], int xy[2], int col)
+{
+	int	bre;
+	int	i;
+
+	i = 1;
+	bre = dxy[0] / 2;
+	while (i < dxy[0])
+	{
+		xy[0] += xyi[0];
+		bre += dxy[1];
+		if (bre >= dxy[0])
+		{
+			bre -= dxy[0];
+			xy[1] += xyi[1];
+		}
+		pixel_put_image(init, xy[0], xy[1], col);
+		i++;
+	}
+}
+
+void	line2(t_env *init, int dxy[2], int xyi[2], int xy[2], int col)
+{
+	int	bre;
+	int	i;
+
+	i = 1;
+	bre = dxy[1] / 2;
+	while (i < dxy[1])
+	{
+		xy[1] += xyi[1];
+		bre += dxy[0];
+		if (bre >= dxy[1])
+		{
+			bre -= dxy[1];
+			xy[0] += xyi[0];
+		}
+		pixel_put_image(init, xy[0], xy[1], col);
+		i++;
+	}
+}
+
+void	line(t_env *init, t_point **tab, int j, int col)
+{
+	int dxy[2];
+	int xyi[2];
+	int xy[2];
+
+	xy[0] = tab[j]->px;
+	xy[1] = tab[j]->py;
+	dxy[0] = tab[j + 1]->px - tab[j]->px;
+	dxy[1] = tab[j + 1]->py - tab[j]->py;
+	xyi[0] = ( dxy[0] > 0 ) ? 1 : -1;
+	xyi[1] = ( dxy[1] > 0 ) ? 1 : -1;
+	dxy[0] = ft_valabs(dxy[0]);
+	dxy[1] = ft_valabs(dxy[1]);
+	if (dxy[0] > dxy[1])
+		line1(init, dxy, xyi, xy, col);
+	else
+		line2(init, dxy, xyi, xy, col);
+}
+
 void	drawline(t_env *init, t_point ***tab, int col)
 {
 	int	i;
 	int	j;
-	int	k;
-	int	l;
-	int	dx;
-	int	dy;
 
 	i = 0;
 	while (i < tab[0][0]->sizeline)
@@ -52,19 +110,7 @@ void	drawline(t_env *init, t_point ***tab, int col)
 		j = 0;
 		while (j < tab[i][0]->sizecol - 1)
 		{
-			k = 0;
-			l = 0;
-			dx = tab[i][j + 1]->px - tab[i][j]->px;
-			dy = tab[i][j + 1]->py - tab[i][j]->py;
-			while (k <= dx)
-			{
-				if (dx != 0)
-					l = (dy * k) / dx;
-				if (col != 0x000000)
-					col = 0xffffff - ((tab[i][j]->z * init->axez));
-				pixel_put_image(init, tab[i][j]->px + k, tab[i][j]->py + l, col);
-				k++;
-			}
+			line(init, tab[i], j, col);
 			j++;
 		}
 		i++;

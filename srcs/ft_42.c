e6ed85635f2931bd8fa4fe4adade19b7719ad231
Alex MOINIER <amoinier@e2r7p4.42.fr>
2016-01-24 20:08:23 +0100

    Yes

--- srcs/ft_42.c
+++ srcs/ft_42.c
@@ -3,36 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_42.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amoinier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: amoinier <amoinier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/22 21:12:40 by amoinier          #+#    #+#             */
-/*   Updated: 2016/01/24 15:58:38 by amoinier         ###   ########.fr       */
+/*   Updated: 2016/01/24 20:07:36 by amoinier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	drawcol(t_env *init, t_point ***tab, int ij[2], int col)
+static	void	drawcol(t_env *init, t_point ***tab, int ij[2], int col)
 {
-	int	i;
-	int	j;
-	
-	i = ij[0];
-	j = ij[1];
-	column(init, tab,ij, col);
+	ij[0]--;
+	column(init, tab, ij, col);
 }
 
-void	drawline(t_env *init, t_point ***tab, int ij[2], int col)
+static	void	drawline(t_env *init, t_point ***tab, int ij[2], int col)
 {
 	int	j;
 	int	i;
 
-	j = ij[1];
+	j = ij[1] - 1;
 	i = ij[0];
 	line(init, tab[i], j, col);
 }
 
-void	draw(t_env *init, t_point ***tab, int col)
+void			draw(t_env *init, t_point ***tab, int col)
 {
 	int	i;
 	int	j;
@@ -45,23 +41,15 @@ void	draw(t_env *init, t_point ***tab, int col)
 		while (j < tab[i][0]->sizecol)
 		{
 			tab[i][j]->px = (init->axex + (tab[i][j]->x * init->zoom));
-			tab[i][j]->px -= ((tab[i][j]->z * init->axez) / 10) - init->movex;
+			tab[i][j]->px -= ((tab[i][j]->z * init->axez)) - init->movex;
 			tab[i][j]->py = (init->axey + (tab[i][j]->y * init->zoom));
-			tab[i][j]->py -= ((tab[i][j]->z * init->axez) / 10) - init->movey;
+			tab[i][j]->py -= ((tab[i][j]->z * init->axez)) - init->movey;
 			ij[0] = i;
 			ij[1] = j;
 			if (j > 0)
-			{
-				ij[1]--;
 				drawline(init, tab, ij, col);
-				ij[1]++;
-			}
 			if (i > 0 && j < tab[i - 1][0]->sizecol)
-			{
-				ij[0]--;
 				drawcol(init, tab, ij, col);
-			}
-			
 			j++;
 		}
 		i++;
